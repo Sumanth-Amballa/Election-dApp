3dbbf8b81ba3964dfa9b4cbe12f220c2c40726e3
Sumanth <69922047+Sumanth-Amballa@users.noreply.github.com>
2020-08-27 13:03:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -12,6 +12,35 @@ function Info2(){
 var userId = document.getElementById('s1').value;
 var passwordId = document.getElementById('s2').value;
 }
+//for web3 and metamask
+window.addEventListener('load', () => {
+ // Wait for loading completion to avoid race conditions with web3 injection timing.
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+      // Acccounts now exposed
+      return web3;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    // Use Mist/MetaMask's provider.
+    const web3 = window.web3;
+    console.log('Injected web3 detected.');
+    return web3;
+  }
+  // Fallback to localhost; use dev console port by default...
+  else {
+    const provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545-');
+    const web3 = new Web3(provider);
+    console.log('No web3 instance injected, using Local web3.');
+    return web3;
+  }
+});
 
 /*class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
